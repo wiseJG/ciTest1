5d3e296ba0efa38e541b6f2b4088d4dc273e95cc
wiseJG <eaddt16@gmail.com>
2024-03-09 00:59:41 +0900

    텍스트 하나 추가합니다.

--- app/src/main/java/gun/jang/citest1/MainActivity.kt
+++ app/src/main/java/gun/jang/citest1/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    var str1 = "기능을 추가 합니다."
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
