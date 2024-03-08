f82af025e296c687fc6fb6f0a446c46012d6dce1
wiseJG <eaddt16@gmail.com>
2024-03-09 01:33:23 +0900

    dev 로 커밋합니다. 스트링 하나 추가요.

--- app/src/main/java/gun/jang/citest1/MainActivity.kt
+++ app/src/main/java/gun/jang/citest1/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 
     var str1 = "기능을 추가 합니다.!!!!"
-
+    var str2 = "기능을 추가 합니다.!!222!"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
